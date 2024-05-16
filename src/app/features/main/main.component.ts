05bd38ef36d2d09f66edae23e86e42f1cb9a43d7
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-05-17 01:11:07 +0400

    Main new section
    added grid section for photos
    added email send service
    added consts folder

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
