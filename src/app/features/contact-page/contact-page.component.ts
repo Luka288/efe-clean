fa3bb2989ef0ce5013c90cc31c6b104bf6dfe31d
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-05-23 23:00:23 +0400

    forms done
    form done,
    form validators done.

--- src/app/features/contact-page/contact-page.component.ts
+++ src/app/features/contact-page/contact-page.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormBuilder,} from '@angular/forms';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { first } from 'rxjs';
 
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonToggleModule],
+  imports: [MatIconModule, MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonToggleModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export default class ContactPageComponent {
+  private readonly fb = inject(FormBuilder)
 
+  readonly signUpForm =this.fb.group({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(16),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    phone: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  })
 }
+
