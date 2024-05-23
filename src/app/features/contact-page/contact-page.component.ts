d7bd6fdcd9281f778bbe85b6bcea7e1fa889ea6a
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-05-23 22:03:29 +0400

    angular materials

--- src/app/features/contact-page/contact-page.component.ts
+++ src/app/features/contact-page/contact-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
-import { FormBuilder, FormsModule, ReactiveFormsModule  } from "@angular/forms";
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormBuilder,} from '@angular/forms';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatButtonToggleModule],
+  imports: [MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonToggleModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
