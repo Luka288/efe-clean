c2bad4e7cca239cb101b64a4d516e0e5d842aee4
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-06-01 00:33:32 +0400

    working on email send

--- src/app/features/contact-page/contact-page.component.ts
+++ src/app/features/contact-page/contact-page.component.ts
@@ -22,34 +22,50 @@ export default class ContactPageComponent {
   private readonly userID = 'M6lXw60kHrTBq4rzm'
 
   readonly signUpForm =this.fb.group({
-    firstName: new FormControl('', [
+    firstName: new FormControl('asdasd', [
       Validators.required,
       Validators.minLength(2),
       Validators.maxLength(16),
     ]),
-    email: new FormControl('', [
+    email: new FormControl('asdasd@gmail.com', [
       Validators.required,
       Validators.email,
     ]),
-    phone: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    description: new FormControl('', [
+    phone: new FormControl('12312321', [Validators.required, Validators.minLength(2)]),
+    description: new FormControl('test test test', [
       Validators.required,
+      Validators.minLength(10)
     ])
   })
 
 
-  emailService(info: Event){
-    emailjs
-    .sendForm('service_j8gjr1z', 'template_skrl3dc', info.target as HTMLFormElement,this.userID)
-    .then(
-      () => {
-        console.log('SUCCESS!');
-      },
-      (error) => {
-        console.log('FAILED...', (error as EmailJSResponseStatus).text);
-      },
-    );
+  
 
-  }
+  // !! finding way to send email working on it ! 
+
+  // emailService(event: SubmitEvent){
+  //   event.preventDefault();
+  //   if(this.signUpForm.valid){
+  //     const values = {
+  //       firstName: this.signUpForm.get('firstName')?.value,
+  //       email: this.signUpForm.get('email')?.value,
+  //       phone: this.signUpForm.get('phone')?.value,
+  //       description: this.signUpForm.get("description")?.value
+  //     }
+
+  //     emailjs
+  //     .send('service_j8gjr1z', 'template_skrl3dc', values, this.userID)
+  //     .then(
+  //       () => {
+  //         console.log(this.signUpForm.value)
+  //         console.log('SUCCESS!',);
+  //       },
+  //       (error) => {
+  //         console.log('FAILED...', (error as EmailJSResponseStatus).text);
+  //       },
+  //     );
+  
+  //   }
+  // }
 }
 
