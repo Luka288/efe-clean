85521afa859e91238fc405baa01dae8fd0e48839
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-06-02 01:06:03 +0400

    Email form page (updates)
    added sweet alert service,
    updated email send form as gmail message (visual),

--- src/app/features/contact-page/contact-page.component.ts
+++ src/app/features/contact-page/contact-page.component.ts
@@ -5,7 +5,7 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
-import { Target } from '@angular/compiler';
+import { SweetAlertService } from '../shared/services/sweet-alert.service';
 
 
 
@@ -18,6 +18,7 @@ import { Target } from '@angular/compiler';
 })
 export default class ContactPageComponent {
   private readonly fb = inject(FormBuilder)
+  private readonly alert = inject(SweetAlertService);
 
   readonly signUpForm =this.fb.group({
     firstName: new FormControl('', [
@@ -39,16 +40,20 @@ export default class ContactPageComponent {
       Validators.minLength(10)
     ])
   })
-
-emailSend(){
-  emailjs.send("service_6maur7m","template_skrl3dc",{
-    from_name: this.signUpForm.value.firstName,
-    Last_name: this.signUpForm.value.lastName,
-    reply_to: this.signUpForm.value.email,
-    user_mobile: this.signUpForm.value.phone,
-    message: this.signUpForm.value.description,
-    });
+ emailSend(){
+  emailjs.init('M6lXw60kHrTBq4rzm')
+  if(this.signUpForm.valid){
+       emailjs.send("service_6maur7m","template_skrl3dc",{
+      from_name: this.signUpForm.value.firstName,
+      Last_name: this.signUpForm.value.lastName,
+      reply_to: this.signUpForm.value.email,
+      user_mobile: this.signUpForm.value.phone,
+      message: this.signUpForm.value.description,
+      });
+    this.alert.alert('Email Sent !', 'success', 'We will contact you soon !')
+  }else{
+    this.alert.alert('Check inputs', 'error', 'there is possibilty that input is not valid')
+  }
 }
-
 }
 
